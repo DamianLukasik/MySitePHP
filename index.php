04ec343d3ec69a2d201df73b4a9243852fe7d04d
Damian Łukasik <Damian.lukasik@mail.com>
2017-02-21 13:28:27 +0100

    problem z kodowaniem

--- index.php
+++ index.php
@@ -24,13 +24,19 @@
 <html>
     <head>
         <title>TODO supply a title</title>
-        <meta charset="UTF-8">
+        <meta http-equiv="content-type" content="text/html; charset=iso-8859-2" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/przycisk.css" rel="stylesheet" type="text/css">
         <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
     </head>
     <body>
-        <?php echo '<div>Program dla pana Zbyszka - Kontrolka do ładowania plków</div><br><br>' ?>
+        <?php 
+        $str = '<div>Program dla pana Zbyszka - Kontrolka do ładowania plków</div><br><br>';
+        
+    //    echo mb_detect_encoding($str)."\t".$str."\n"; 
+        echo $str;
+        
+        ?>
         
         <form name="uploadForm" action="index.php" method="POST" enctype="multipart/form-data">         
             <div class="browse-wrap">
@@ -39,7 +45,7 @@
            <!-- -->   <input type="submit" value="Odczytaj" name="submit" class="title" style="visibility: collapse"/> 
             </div>
             <span class="upload-path"></span>                 
-            <script type="text/javascript">
+            <script type="text/javascript">                
                 // Span
                 var span = document.getElementsByClassName('upload-path');
                 // Button
@@ -54,42 +60,115 @@
                         var fileDisplayArea = document.getElementById('fileDisplayArea');
                         var textType = /text.*/;
                         var file = [];
-                        for(var x=0;x<this.files.length;x++)
-                        {
-                            span[x].innerHTML = this.files[x].name;      
-                            file.push(this.files[x]);
-                            
-                        }       
-                        if (file.type.match(textType)) {
-                            var reader = new FileReader();
-                            reader.onload = function(e) {
+                        var _s; 
+                        var s_;
+                        var str = "q=";
+                                              
+                      //  alert(this.files[1].name+"   "+this.files.length);
+                     /*   for(var x=0;x<this.files.length;x++){
+                           // alert(this.files[x].name+"  "+x);
+                           // setTimeout(g, 1000);
+                       //     span[0].innerHTML = this.files[x].name;  
+                          //  alert();    
+                            file.push(this.files[x]); 
+                        }*/
+                        
+                          //  if(file[x].type.match(textType))
+                         //   {
+                             //   var reader = new FileReader();  
+                              //  alert(file[x]);
+                              //  setTimeout(g, 1000);
+                            /*    reader.onload = function(e) {
+                                //  alert(reader.result);
+                                    var s  = reader.result.split("\n"); 
+                                    _s = "";  
+                                    for(var u=0; u<s.length; u++)
+                                    { 
+                                    //    alert(s[u]);
+                                        var a = s[u];                               
+                                        for (n = 1; n < a.length; ++n)
+                                        {
+                                         //   alert(a[n]);
+                                            _s += a[n];
+                                        }   
+                                        _s +="\n";
+                                    }     
+                                //    str += reader.result+"\n";  
+                                    str += _s;                      
+                                //    alert(str);
+                                };*/
+                           //     reader.readAsText(file);  
                                 
-                                xmlHTTP = new XMLHttpRequest();
-                                                                
-                                var str = reader.result;
-                                str = "q=" + str;//encodeURIComponent(str);
-                                alert(str);
-                                
-                                xmlHTTP.open("POST", "tabelka.php", true);
-                                xmlHTTP.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                                xmlHTTP.setRequestHeader("Content-length", str.length);
-                                xmlHTTP.setRequestHeader("Connection", "close");
-                                xmlHTTP.onreadystatechange = function() {
-                                    //Call a function when the state changes.
-                                    if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200) {
-                                        document.write(xmlHTTP.responseText);                                        
-                                    }
+                                var j = 0, k = this.files.length;
+                                for (var i = 0; i < k; i++) {
+                                    var reader = new FileReader();
+                                 //   alert();
+                                     reader.onloadend = function (evt) {
+                                        if (evt.target.readyState == FileReader.DONE) {
+                                            
+                                         //   alert(evt.target.result+"");
+                                            
+                                           // data["File_Content" + j] = evt.target.result;
+                                            str += evt.target.result+"\n";
+                                            
+                                            j++;
+                                        /*    if (j == k){
+                                                alert('All files read');
+                                            }*/
+                                        }
+                                    };
+                                    reader.readAsBinaryString(this.files[i]);
                                 }
-                                xmlHTTP.send(str);    
+                                /*
                                 
+                                
+                                var j = 0, k = files.length;
+                                for (var i = 0; i < k; i++) {
+                                    var reader = new FileReader();
+                                    reader.onloadend = function (evt) {
+                                        if (evt.target.readyState == FileReader.DONE) {
+                                            data["File_Content" + j] = btoa(evt.target.result);
+                                            j++;
+                                            if (j == k){
+                                                alert('All files read');
+                                            }
+                                        }
+                                    };
+                                    reader.readAsBinaryString(files[i]);
+                                }
+                                
+                                */
+                              //  alert(_s);                                
+                              //  alert(str);   
+                         /*   }
+                            else 
+                            {
+                                fileDisplayArea.innerText = "File not supported!";
+                            }*/
+                           // alert(x);
+                    //    }
+                        
+                        xmlHTTP = new XMLHttpRequest();  
+                        
+                     //   alert(str);
+                        
+                        xmlHTTP.open("POST", "tabelka.php", true);
+                        xmlHTTP.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                        xmlHTTP.setRequestHeader("Content-length", str.length);
+                        xmlHTTP.setRequestHeader("Connection", "close");
+                        xmlHTTP.onreadystatechange = function() {
+                            //Call a function when the state changes.
+                            if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200) {
+                                document.write(xmlHTTP.responseText);
                             }
-                            reader.readAsText(file);    
-                        } 
-                        else 
-                        {
-                            fileDisplayArea.innerText = "File not supported!"
+                        };
+                        function f() {
+                        //    alert(str);
+                            xmlHTTP.send(str);
                         }
-                    }
+                        setTimeout(f, 800);
+                      //  xmlHTTP.send(str);
+                    };
                 }    
             </script>                
             <!-- 
--- tabelka.php
+++ tabelka.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>TODO supply a title</title>
-        <meta charset="UTF-8">
+        <meta http-equiv="content-type" content="text/html; charset=iso-8859-2" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/przycisk.css" rel="stylesheet" type="text/css">
         <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
@@ -19,7 +19,11 @@
     <body>
         <?php echo 'Tabelka z produktami<br>'; ?>
 
-        <?php
+        <?php       
+         /*   date_default_timezone_set('Europe/Warsaw');
+            mb_internal_encoding('UTF-8');
+            setlocale(LC_ALL, 'pl_PL.UTF-8','pl.UTF-8','pol.UTF-8','plk.UTF-8','polish.UTF-8','poland.UTF-8');
+            header('Content-type: text/html; charset=utf-8');*/
             if(isset($_POST['q'])){
 
                 echo "<table>"
@@ -30,18 +34,36 @@
                         . "<td> </td>"
                     . "</tr></tfoot>";*/
                                 
-                $czesci = explode("\n", $_POST['q']);
+                $czesci = explode("\n", $_POST['q']);     
+                
+            //    echo mb_detect_encoding($czesci[0])."\t".$czesci[0]."\n";
+                
+            //    $czesci[0] = iconv(mb_detect_encoding($czesci[0]), "ISO-8859-2//IGNORE",$czesci[0]);
+                        
+           //     echo $czesci[0].'';
                 
                 for($i = 0, $count = count($czesci); $i < $count; $i++)
-                {
+                {                    
                     $str = $czesci[$i];
-                    $tab = explode("|",$str);  
+                    $tab = explode("|",$str); 
+                    //problem z kodowaniem
+                 //   echo mb_detect_encoding($tab[0])."\t".$tab[0]."\n";
+                    echo mb_detect_encoding($tab[1])."\t".$tab[1]."\n";
+                  //  echo mb_detect_encoding($tab[2])."\t".$tab[2]."\n";
+                    
+                //    $tab[0] = iconv(mb_detect_encoding($tab[0]), "utf-8//IGNORE",$tab[0]); 
+                    $tab[1] = iconv(mb_detect_encoding($tab[1]), "iso-8859-2//IGNORE",$tab[1]); 
+                 //   $tab[2] = iconv(mb_detect_encoding($tab[2]), "utf-8//IGNORE",$tab[2]); 
+                 //   
+                    echo mb_detect_encoding($tab[1])."\t".$tab[1]."\n";
+                    
                     $str = "<tr><td>".$tab[0]."</td><td>".$tab[1]."</td><td>".$tab[2]."</td></tr>";
-                    $str = iconv("utf-8", "iso-8859-2//IGNORE", $str);  
+                 //   $str = iconv(mb_detect_encoding($str), "utf-8//IGNORE",$str); 
+                    
+                    
+                    
                     echo $str;
                 }
-
-                
                 echo "</td></tr></table>";
             }
         ?>
